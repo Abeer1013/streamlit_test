d7a42579bcc19a20d593f5d71f558a64f5dcd9f1
Abeer1013 <145390166+Abeer1013@users.noreply.github.com>
2023-11-25 21:53:56 +0300

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -1,57 +1,50 @@
-
+# Streamlit Documentation: https://docs.streamlit.io
 import streamlit as st
 import pandas as pd
-import numpy as np
-from PIL import Image
-
 import pickle
 
-html_temp = """
-<div style="background-color:yellow;padding:1.5px">
-<h1 style="color:black;text-align:center;">Used Car Price Prediction</h1>
-</div><br>"""
-st.markdown(html_temp, unsafe_allow_html=True)
+# Title/Text
+st.title("Atoscout Project")
+st.write("Welcome to Car Price Prediction Project.")
 
+# Load the dataset
+df = http://pd.read_csv("Ready_to_ML (1).csv")
 
-st.write("\n\n"*2)
+# Display dataframe
+st.header("Dataset Overview:")
+st.dataframe(df.describe().T)
 
-filename = 'Auto_Price_Pred_Model'
-model = pickle.load(open(filename, 'rb'))
+# Load machine learning model
+filename = "my_model_Last"
+model = pickle.load(open(filename, "rb"))
 
-with st.sidebar:
-    st.subheader('Car Specs to Predict Price')
+# User Inputs
+st.sidebar.title("Feature Inputs:")
+typee = st.sidebar.selectbox("Choose the type:", ["Used", "Pre-registered", "Demonstration", "Employee's car"])
+make_model = st.sidebar.selectbox("Choose the make_model:", df["make_model"].unique())
 
-make_model = st.sidebar.selectbox("Model Selection", ("Audi A3", "Audi A1", "Opel Insignia", "Opel Astra", "Opel Corsa", "Renault Clio", "Renault Espace", "Renault Duster"))
-hp_kW = st.sidebar.number_input("Horse Power:",min_value=40, max_value=294, value=120, step=5)
-age = st.sidebar.number_input("Age:",min_value=0, max_value=3, value=0, step=1)
-km = st.sidebar.number_input("km:",min_value=0, max_value=317000, value=10000, step=5000)
-Gears = st.sidebar.number_input("Gears:",min_value=5, max_value=8, value=5, step=1)
-Gearing_Type = st.sidebar.radio("Gearing Type", ("Manual", "Automatic", "Semi-automatic"))
+power_kW = st.sidebar.number_input("Power (kW):")
+engine_size = st.sidebar.number_input("Engine Size:")
+mileage = st.sidebar.number_input("Mileage:")
+age = st.sidebar.number_input("Age:")
 
+# Create a dataframe using feature inputs
+user_inputs = pd.DataFrame({
+    "type": [typee],
+    "make_model": [make_model],
+    "power_kW": [power_kW],
+    "engine_size": [engine_size],
+    "mileage": [mileage],
+    "age": [age],
+})
 
-my_dict = {"make_model":make_model, "hp_kW":hp_kW, "age":age, "km":km, "Gears":Gears, "Gearing_Type":Gearing_Type}
-df = pd.DataFrame.from_dict([my_dict])
+# Display user inputs
+st.header("User Inputs:")
+st.table(user_inputs)
 
-cols = {
-    "make_model": "Car Model",
-    "hp_kW": "Horse Power",
-    "age": "Age",
-    "km": "km Traveled",
-    "Gears": "Gears",
-    "Gearing_Type": "Gearing Type"
-}
+# Prediction with user inputs
+predict = st.button("Predict")
 
-df_show = df.copy()
-df_show.rename(columns = cols, inplace = True)
-st.write("Selected Specs: \n")
-st.table(df_show)
-
-
-if st.button("Predict"):
-    pred = model.predict(df)
-    col1, col2 = st.columns(2)
-    col1.write("The estimated value of car price is €")
-    col2.write(pred[0].astype(int))
-
-
-st.write("\n\n")
+if predict:
+    prediction = model.predict(user_inputs)
+    st.success(f"Predicted Price: {prediction[0]:,.2f} $")
